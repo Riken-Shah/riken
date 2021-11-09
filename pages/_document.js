828a17f4d35dc649b5184aca9fca6b379b7bb74e
Riken Shah <rikenshah.02@gmail.com>
2021-11-09 14:34:07 +0530

    feat: added more font size for Montserrat

--- pages/_document.js
+++ pages/_document.js
@@ -63,7 +63,7 @@ export default class extends Document {
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
           <link
-            href="https://fonts.googleapis.com/css2?family=Montserrat:wght@100;600&display=swap"
+            href="https://fonts.googleapis.com/css2?family=Montserrat:wght@100;200;300;400;500;600;700;800;900&display=swap"
             rel="stylesheet"
           />
         </Head>
